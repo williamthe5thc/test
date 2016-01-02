b3fde1ccb2473be0b20adb28e6a56571303379db
williamthe5thc <williamthe5thc@icloud.com>
2016-01-02 14:56:34 -0800

    xcode git testing
    xcode git testing stuff

---
+++ test2 git/test2 git/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test2 git
+//
+//  Created by Jordan Charles on 1/2/16.
+//  Copyright © 2016 Jordan Charles. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
